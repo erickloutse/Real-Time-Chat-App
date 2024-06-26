254193f89e7d2b06c4009fe74f4c7231efb4744f
erickloutse <erickloutse@gmail.com>
2024-06-26 15:50:01 +0000

    Chat-App

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -10,11 +12,12 @@ const PORT = process.env.PORT || 5001;
 dotenv.config();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-  // root rout http://localhost:5001/
-
+// root rout http://localhost:5001/
 
 app.listen(PORT, () => {
   connectToMongoDB();
