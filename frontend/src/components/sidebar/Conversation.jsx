0843f027f7b628aa7e690b6a1ceb264d52c39995
erickloutse <erickloutse@gmail.com>
2024-07-09 01:54:34 +0000

    ChatApp Update

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,16 +1,19 @@
-import useConversation from "../../zustand/useConversation";
 import { useSocketContext } from "../../context/SocketContext";
+import useConversation from "../../zustand/useConversation";
 
 const Conversation = ({ conversation, lastIdx, emoji }) => {
   const { selectedConversation, setSelectedConversation } = useConversation();
+
   const isSelected = selectedConversation?._id === conversation._id;
   const { onlineUsers } = useSocketContext();
   const isOnline = onlineUsers.includes(conversation._id);
+
   return (
     <>
       <div
-        className={`flex gap-2 items-center hover:bg-green-500 rounded p-2 py-1 cursor-pointer 
-        ${isSelected ? "bg-green-500" : ""}`}
+        className={`flex gap-2 items-center hover:bg-green-500 rounded p-2 py-1 cursor-pointer
+				${isSelected ? "bg-green-500" : ""}
+			`}
         onClick={() => setSelectedConversation(conversation)}
       >
         <div className={`avatar ${isOnline ? "online" : ""}`}>
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,10 +1,10 @@
-import Slidebar from "../../components/slidebar/Slidebar";
+import Sidebar from "../../components/sidebar/Sidebar";
 import MessageContainer from '../../components/messages/MessageContainer';
 
 const Home = () => {
   return (
     <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
-      <Slidebar />
+      <Sidebar />
       <MessageContainer/>
     </div>
   );
